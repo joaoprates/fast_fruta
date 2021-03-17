fc0bf6fed04f1a6587a9f7f09a23f524dda2f8ef
João Prates <jovictor833@gmail.com>
2021-03-17 20:37:45 -0300

    first commit

---
+++ FastFruta/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FastFruta.MvcApplication" Language="C#" %>
---
+++ FastFruta/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FastFruta.Startup))]
+namespace FastFruta
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
